684335f0d49265cc88d5d21534e9d1289cacc371
0aaryan <aroraryan826@gmail.com>
2024-07-13 15:56:24 +0530

    changed db usage type

--- home/admin.py
+++ home/admin.py
@@ -65,6 +65,6 @@ class StockAdmin(admin.ModelAdmin):
                 messages.error(request, f"Error updating stock: {stock}")
                 continue
         messages.success(request, 'Stocks updated successfully')
-        return redirect('admin:home')
+        return redirect('admin')
     
 admin.site.register(Stock, StockAdmin)
